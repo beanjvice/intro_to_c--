e71deb672c41a16336e2134e3654f2d51727c5a1
Arisa Moonsammy <aricollegem@gmail.com>
2025-09-04 14:48:22 -0400

    modified hw one and program 1 file

--- intro_c++_fall2025/program1.cpp
+++ intro_c++_fall2025/program1.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout << "Thursday September 4th, 2025"; // << insertion operator
     cout << endl;
+    cout << "Thursday September 4th, 2025" << endl << "CST2403" << endl;
     // cout stands for console output
 }
 
