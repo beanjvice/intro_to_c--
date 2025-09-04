5378c00d9d85788770a55b7ba07317587fce57ad
Arisa Moonsammy <aricollegem@gmail.com>
2025-09-04 14:43:12 -0400

    making proper repo, homework one, in class one

---
+++ intro_c++_fall2025/program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//comment
+using namespace std;
+int main()
+{
+    cout << "Thursday September 4th, 2025"; // << insertion operator
+    cout << endl;
+    // cout stands for console output
+}
+
+// g++ -std=c++17 -Wall hw.cpp -o hw.exe
